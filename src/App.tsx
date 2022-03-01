9030589c5df2284a1f26483c86472c58c67bd5ca
creanick <creanickofficial@gmail.com>
2022-03-01 12:19:34 +0530

    added color changing button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import styled from "@emotion/styled";
+const Button = styled.button<{ count: number }>`
+  background-color: ${(props) =>
+    `rgb(${props.count - 255},${props.count},${props.count - 255})`};
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  border-radius: 4px;
+  cursor: pointer;
+  &:active {
+    transform: scale(0.95);
+  }
+`;
 function App() {
   const [count, setCount] = useState(0);
+  const increment = () => {
+    if (count >= 255) {
+      setCount(0);
+    } else {
+      setCount(count + 1);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + Manick!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <Button count={count} type="button" onClick={increment}>
             count is: {count}
-          </button>
+          </Button>
         </p>
       </header>
     </div>
